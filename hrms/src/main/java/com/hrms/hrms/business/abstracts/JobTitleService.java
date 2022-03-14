5915c106c2d865b10424da4cf290ff7692781fc3
ONUR <ONUR@DESKTOP-36KJSTU>
2022-03-14 21:33:44 +0300

    Ödev Update

---
+++ hrms/src/main/java/com/hrms/hrms/business/abstracts/JobTitleService.java
@@ -0,0 +1,11 @@
+package com.hrms.hrms.business.abstracts;
+
+import java.util.List;
+
+import com.hrms.hrms.entities.concretes.JobTitle;
+
+public interface JobTitleService {
+	
+	List<JobTitle> getAll();
+
+}
